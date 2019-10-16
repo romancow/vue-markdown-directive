43075031b717211f2e6d73214e2aacba5eb5b2b5
Sean Wilson <seancwilson@gmail.com>
2019-10-16 11:45:04 -0500

    allow preset to be set w/ directive argument and boolean options w/ modifiers

--- index.ts
+++ index.ts
@@ -6,13 +6,28 @@ export enum MarkdownItPreset {
 	Zero = 'zero',
 	CommonMark = 'commonmark'
 }
+
 type VueMarkdownDirective = DirectiveOptions & { preset?: MarkdownItPreset,  options?: MarkdownIt.Options }
+
+const isPreset = (arg: string | undefined): arg is MarkdownItPreset =>
+	(arg != null) && Object.values<string>(MarkdownItPreset).includes(arg)
+
+const toOptions = (modifiers: { [key: string]: boolean }, defaults: MarkdownIt.Options = {}) => {
+	const options = Object.assign({}, defaults)
+	return Object.keys(modifiers).reduce((opts, key) => {
+		const [opt, value] = (key.substring(0, 3) === "no-") ? [key.substring(3), false] : [key, true]
+		opts[opt] = value
+		return opts
+	}, options as { [key: string]: boolean }) as MarkdownIt.Options
+}
 
 const vueMarkdownDirective: VueMarkdownDirective = {
 
 	inserted(el, {value, arg, modifiers}) {
 		const { preset = MarkdownItPreset.Default,  options } = this
-		const markdownIt = MarkdownIt(preset, options)
+		const presetName = isPreset(arg) ? arg : preset
+		const mdOptions = toOptions(modifiers, options)
+		const markdownIt = MarkdownIt(presetName, mdOptions)
 		const md = (value != null) ? value : el.innerHTML
 		markdownIt.render(md)
 	}
